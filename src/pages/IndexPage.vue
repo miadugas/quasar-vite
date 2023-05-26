62729d2c3c575167dacd333cb2dbd44b1858582f
Mia Dugas <miadugas@Mias-MacBook-Air.local>
2023-05-26 10:05:38 -0600

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-img src="https://picsum.photos/1000/500" />
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorum dolor minus sed magni maxime ut placeat at, laboriosam aliquid eius tempore debitis quae quidem rem. Maxime fugit esse architecto!
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-img src="https://picsum.photos/1000/500" />
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorum dolor minus sed magni maxime ut placeat at, laboriosam aliquid eius tempore debitis quae quidem rem. Maxime fugit esse architecto!
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
